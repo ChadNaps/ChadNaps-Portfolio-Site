f3e2bd4affe9d217c1dc56c4fb49925f7508adfa
ChadNaps <napperc@hotmail.com>
2021-06-15 12:17:37 -0600

    Create route to old version of site

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Select Site Version' });
 });
 
+/* GET old site. */
+router.get('/old', (req, res, next) => {
+  res.sendFile(req.app.locals.path.join(req.app.locals.__dirname, 'site.old/index.html'));
+});
+
 export default router;
