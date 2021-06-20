87b1fd6d88247da2cd4d4a9cc4d9008cf9acca46
ChadNaps <napperc@hotmail.com>
2021-06-20 17:42:17 -0600

    Add projects navigation logic

---
+++ public/scripts/index.js
@@ -0,0 +1,9 @@
+const projectsBtn = document.getElementsByClassName('nav-projects');
+const aboutmeBtn = document.getElementsByClassName('nav-aboutme');
+const socialsBtn = document.getElementsByClassName('nav-socials');
+
+for (const element of projectsBtn) {
+    element.addEventListener("click", () => {
+        window.location += "/projects";
+    });
+}
