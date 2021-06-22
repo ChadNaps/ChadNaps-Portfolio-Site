9cd3dee17f3a91b33e9ddaf2a18f72e6297e5c92
ChadNaps <napperc@hotmail.com>
2021-06-21 19:18:27 -0600

    Add initial layout and styling for projects page

--- models/projects.js
+++ models/projects.js
@@ -13,6 +13,21 @@ const projects = [
             "name": "Project 3",
             "img": "/another.jpg",
             "description": "The third one looked like this! T'was nice."
+        },
+        {
+            "name": "Project 4",
+            "img": "/a_url/here/yay.png",
+            "description": "This is my first project! It's great!"
+        },
+        {
+            "name": "Project 5",
+            "img": "/another/url/wheeeew.jpg",
+            "description": "Then I did this second project! It was great! You'll love it!"
+        },
+        {
+            "name": "Project 6",
+            "img": "/another.jpg",
+            "description": "The third one looked like this! T'was nice."
         }
     ];
 
