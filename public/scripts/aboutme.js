8d4adb005058c12b32fbc5dad0c49c9838b1daff
ChadNaps <napperc@hotmail.com>
2021-07-08 14:34:05 -0600

    Update aboutme clock
    Allows clock to update the time without refreshing.

--- public/scripts/aboutme.js
+++ public/scripts/aboutme.js
@@ -1,8 +1,33 @@
 const currentTimeSpan = document.getElementById("currentTime");
 
-// Cool little current time thingy
-const date = new Date();
-let hours = date.getHours() % 12;
-let minutes = ('0' + date.getMinutes()).slice(-2);
+// Cool little current time thingy //
+let currentTime = getTime();
+currentTimeSpan.innerHTML = `${currentTime.hours}:${currentTime.minutes}`;
 
-currentTimeSpan.innerHTML = `${hours}:${minutes}`;
+// Update time regularly //
+setInterval(() => {
+    if (timeHasChanged(currentTime.hours, currentTime.minutes)) {
+        currentTime = getTime();
+
+        currentTimeSpan.innerHTML = `${currentTime.hours}:${currentTime.minutes}`;
+    }
+}, 1000);
+
+// Helper functions //
+function timeHasChanged(hrs, mins) {
+    const newCurrentTime = getTime();
+    if (newCurrentTime.hours != hrs || newCurrentTime.minutes != mins) {
+        return true;
+    }
+
+    return false;
+}
+
+// Get formatted time object
+function getTime() {
+    const tempDate = new Date();
+    return {
+        hours: tempDate.getHours() % 12,
+        minutes: ('0' + tempDate.getMinutes()).slice(-2)
+    }
+}
