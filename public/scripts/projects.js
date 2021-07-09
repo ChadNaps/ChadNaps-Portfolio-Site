0d9e51f0df5f683ba7999b018967f4721ca517b7
ChadNaps <napperc@hotmail.com>
2021-07-09 15:58:00 -0600

    Update project cards color scheme

--- public/scripts/projects.js
+++ public/scripts/projects.js
@@ -1,11 +1,16 @@
 const projects = document.getElementsByClassName("card");
+const projectTitle = document.getElementsByClassName("card-title");
+const projectViewSimple = document.getElementsByClassName("view-simple");
+const projectViewAdvanced = document.getElementsByClassName("view-advanced");
 const toggleViewButton = document.getElementById("toggle-view-button");
 
 // Add progressive opacity changes to each project
 for (let x = 0; x < projects.length; x++) {
     // Calculate alpha based on number of projects
-    const alpha = 1 - (x * (1 / projects.length));
-    projects[x].style.backgroundColor = `hsla(240, 100%, 30%, ${alpha})`;
+    const alpha = 1 - (x * (1 / (projects.length + 2)));
+    projectTitle[x].style.backgroundColor = `hsla(240, 100%, 30%, ${alpha})`;
+    projectViewSimple[x].style.backgroundColor = `hsla(0, 0%, 30%, ${alpha})`;
+    projectViewAdvanced[x].style.backgroundColor = `hsla(0, 0%, 30%, ${alpha})`;
 }
 
 // Add click event listeners to projects
