11c4c38be27cb92e5cb287c43fad3ec7e2ca296f
ChadNaps <napperc@hotmail.com>
2021-07-12 20:03:20 -0600

    Add reaction timer project

--- models/projects.js
+++ models/projects.js
@@ -3,9 +3,17 @@ const projects = [
             "name": "Generic Software Receptionist",
             "img": "/images/GSWR.png",
             "URL": "https://github.com/ChadNaps/generic-software-receptionist",
-            "descriptionSimple": "Scheduling software for a sole proprietorship.",
+            "descriptionSimple": "Scheduling software for a sole proprietorship",
             "descriptionAdvanced": "Open source (MIT License) software built in Node.js using SQLite3 to \
-                                    enable a sole proprietor to offer online scheduling to their clients."
+                                    enable a sole proprietor to offer online scheduling to their clients"
+        },
+        {
+            "name": "Reaction Timer",
+            "img": "/images/reaction-timer-project-preview.png",
+            "URL": "",
+            "descriptionSimple": "A simple game to test your reflexes",
+            "descriptionAdvanced": "This simple game uses VanillaJS to generate a clickable <div> with a random \
+                                    border-radius, color, size, and position set to a timer"
         }
     ];
 
