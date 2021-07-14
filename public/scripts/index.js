f4de8b6189b5d3817b548c4b06c77f8d1352f49f
ChadNaps <napperc@hotmail.com>
2021-07-14 09:38:11 -0600

    Refactor index nav buttons

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,10 +1,24 @@
-const projectsBtn = document.getElementsByClassName('nav-projects');
-const aboutmeBtn = document.getElementsByClassName('nav-aboutme');
-const socialsBtn = document.getElementsByClassName('nav-socials');
+const navBtns = document.getElementsByClassName('nav-container');
 
-for (const element of projectsBtn) {
-    element.addEventListener("click", () => {
-        window.location = "/projects";
+for (let buttonNumber = 0; buttonNumber < navBtns.length; buttonNumber++) {
+    let label = "";
+    for (let childNumber = 0; childNumber < navBtns[buttonNumber].children.length; childNumber++) {
+        for (let grandchildNumber = 0; grandchildNumber < navBtns[buttonNumber].children[childNumber].children.length; grandchildNumber++) {
+            const tempLabel = navBtns[buttonNumber].children[childNumber].children[grandchildNumber].innerHTML;
+
+            if (tempLabel == "Projects" ||
+                tempLabel == "About Me" ||
+                tempLabel == "Socials") {
+                label = tempLabel;
+            }
+        }
+    }
+    
+    if (label == "Projects") {
+        // Click Event Listener
+        navBtns[buttonNumber].addEventListener("click", () => {
+            // window.location = "/projects";
+        });
     });
 }
 
@@ -14,8 +28,18 @@ for (const element of aboutmeBtn) {
     });
 }
 
-for (const element of socialsBtn) {
-    element.addEventListener("click", () => {
+        navBtns[buttonNumber].addEventListener("mouseup", function () {
+            console.log("Mouse up...");
+        });
+
+    } else if (label == "About Me") {
+        navBtns[buttonNumber].addEventListener("click", () => {
+            window.location = "/about%20me";
+        });
+    } else if (label == "Socials") {
+        navBtns[buttonNumber].addEventListener("click", () => {
         window.location = "/socials";
     });
+    } else {
+        console.error(`Path ${label} not found!`);
 }
