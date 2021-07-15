a6e7dbd84ce2cdf92c47d8448c31046d26be5f93
ChadNaps <napperc@hotmail.com>
2021-07-15 11:01:52 -0600

    Delete old website from project

--- app.js
+++ app.js
@@ -35,7 +35,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'site.old'), { index: false }));
 
 // Route Definitions
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'ChadNaps | Home' });
 });
 
-/* GET old site. */
-router.get('/old', (req, res, next) => {
-  res.sendFile(req.app.locals.path.join(req.app.locals.__dirname, 'site.old/index.html'));
-});
-
 export default router;
