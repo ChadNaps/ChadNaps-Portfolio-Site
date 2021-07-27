ChadNaps/ChadNaps-Portfolio-Site

commit 6dffbefe064d3186c8d82e07c0988cacd6ccf6eb

Refactor card colors for easier theme changing

public/scripts/projects.js
@@ -8,9 +8,19 @@ const toggleViewButton = document.getElementById("toggle-view-button");
 for (let x = 0; x < projects.length; x++) {
     // Calculate alpha based on number of projects
     const alpha = 1 - (x * (1 / (projects.length + 2)));
-    projectTitle[x].style.backgroundColor = `hsla(240, 100%, 30%, ${alpha})`;
-    projectViewSimple[x].style.backgroundColor = `hsla(0, 0%, 30%, ${alpha})`;
-    projectViewAdvanced[x].style.backgroundColor = `hsla(0, 0%, 30%, ${alpha})`;
+    
+    // Get default colors
+    let titleColor = getComputedStyle(projectTitle[x]).getPropertyValue('--projects-cards-title');
+    let descriptionColor = getComputedStyle(projectViewSimple[x]).getPropertyValue('--projects-cards-description');
+    
+    // Alter alpha of current colors
+    titleColor = changeAlpha(titleColor, alpha);
+    descriptionColor = changeAlpha(descriptionColor, alpha);
+
+    // Apply colors to cards
+    projectTitle[x].style.setProperty('--projects-cards-title', titleColor);
+    projectViewSimple[x].style.setProperty('--projects-cards-description', descriptionColor);
+    projectViewAdvanced[x].style.setProperty('--projects-cards-description', descriptionColor);
 }
 
 // Add click and hover event listeners to projects
@@ -63,6 +73,20 @@ function swapColors(elements) {
     elements[2].style.backgroundColor = elements[1].style.backgroundColor;
 
     return elements;
+}
+
+function changeAlpha(originalValue, newAlpha) {
+    // Check for proper input
+    if (originalValue.slice(1, 5) != "hsla") {
+        console.error(`Improper value for card header/description. 
+        Received ${originalValue.slice(1, 5)}(), was expecting hsla().`);
+
+        return;
+    }
+
+    const newValue = originalValue.slice(0, -2) + newAlpha.toString() + ')';
+
+    return newValue;
 }
 
 // Logic for advanced view toggle is in global.js

public/stylesheets/projects.css
@@ -63,6 +63,7 @@ button {
 .card h3 {
     padding: 1rem 0;
     flex-basis: 20px;
+    background-color: var(--projects-cards-title);
 }
 
 .card > img {
@@ -72,6 +73,7 @@ button {
 .card > p {
     padding: 1rem 0;
     flex-grow: 3;
+    background-color: var(--projects-cards-description);
 }
 
 .card > * {

public/stylesheets/theme.css
@@ -31,6 +31,8 @@
     --aboutme-card-1: var(--tertiary-light);
     --aboutme-card-2: var(--tertiary-medium);
     --aboutme-card-3: var(--tertiary-darkest);
+    --projects-cards-title: hsla(240, 100%, 30%, 1);
+    --projects-cards-description: hsla(0, 0%, 30%, 1);
 
     /************
     *** Buttons

commit bc14626943f3ce06f8ec68706f2f9841bb1c1051

Fix card coloring that was broken by commit 6dffbef
Card "swap color on hover" effect was broken, specifically.

public/scripts/projects.js
@@ -68,9 +68,18 @@ for (let project of projects) {
 
 // Helper Functions
 function swapColors(elements) {
-    elements[1].style.backgroundColor = elements[0].style.backgroundColor;
-    elements[0].style.backgroundColor = elements[2].style.backgroundColor;
-    elements[2].style.backgroundColor = elements[1].style.backgroundColor;
+    const title = getComputedStyle(elements[0]).getPropertyValue("--projects-cards-title");
+    const desc = getComputedStyle(elements[1]).getPropertyValue("--projects-cards-description");
+
+    if (getComputedStyle(elements[0]).getPropertyValue("--projects-cards-title") == title) {
+        elements[0].style.setProperty("--projects-cards-title", desc);
+        elements[1].style.setProperty("--projects-cards-description", title);
+        elements[2].style.setProperty("--projects-cards-description", title);
+    } else {
+        elements[0].style.setProperty("--projects-cards-title", title);
+        elements[1].style.setProperty("--projects-cards-description", desc);
+        elements[2].style.setProperty("--projects-cards-description", desc);
+    }
 
     return elements;
 }