21cf74983209eb38582ace12acc492abf26930cf
ChadNaps <napperc@hotmail.com>
2021-08-13 10:46:10 -0600

    Improve hover transition on aboutme cards
    Previously they would clip to front, now they fade.

--- public/scripts/aboutme.js
+++ public/scripts/aboutme.js
@@ -1,4 +1,6 @@
 const currentTimeSpan = document.getElementById("currentTime");
+const card2 = document.getElementById("card2");
+const card3 = document.getElementById("card3");
 
 // Advanced view toggle is found in global.js //
 
@@ -14,6 +16,34 @@ setInterval(() => {
         currentTimeSpan.innerHTML = `${currentTime.hours}:${currentTime.minutes}`;
     }
 }, 1000);
+
+// Card Hover Effects
+// Card 2
+let card2Clone = card2.cloneNode(true);
+card2Clone.id = "card2Clone";
+card2.parentElement.appendChild(card2Clone);
+card2Clone.addEventListener("mouseover", () => {
+    card2Clone.style.opacity = 1;
+    card2Clone.style.transition = "opacity ease 0.6s"
+});
+card2Clone.addEventListener("mouseleave", () => {
+    card2Clone.style.opacity = 0;
+    card2Clone.style.transition = "opacity ease 0.6s"
+});
+
+// Card 3
+let card3Clone = card3.cloneNode(true);
+card3Clone.id = "card3Clone";
+card3.parentElement.appendChild(card3Clone);
+card3Clone.addEventListener("mouseover", () => {
+    card3Clone.style.opacity = 1;
+    card3Clone.style.transition = "opacity ease 0.6s"
+});
+card3Clone.addEventListener("mouseleave", () => {
+    card3Clone.style.opacity = 0;
+    card3Clone.style.transition = "opacity ease 0.6s"
+});
+
 
 // Helper functions //
 function timeHasChanged(hrs, mins) {
