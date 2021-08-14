806272954dc8fc5c3390c4e0377a2c72c9e465d8
ChadNaps <napperc@hotmail.com>
2021-08-14 17:19:40 -0600

    Fix socials links after adding cards

--- public/scripts/socials.js
+++ public/scripts/socials.js
@@ -1,43 +1,15 @@
-const email = [
-    document.getElementById("logo-email"),
-    document.getElementById("text-email")
-];
+document.getElementById("card-email").addEventListener("click", () => {
+    window.location = "mailto:contact@chadnaps.com";
+});
 
-const twitter = [
-    document.getElementById("logo-twitter"),
-    document.getElementById("text-twitter")
-];
-
-const github = [
-    document.getElementById("logo-github"),
-    document.getElementById("text-github")
-];
-
-const linkedin = [
-    document.getElementById("logo-linkedin"),
-    document.getElementById("text-linkedin")
-];
-
-for (child of email) {
-    child.addEventListener("click", () => {
-        parent.location = "mailto:contact@chadnaps.com";
-    });
-};
-
-for (child of twitter) {
-    child.addEventListener("click", () => {
+document.getElementById("card-twitter").addEventListener("click", () => {
         window.open("https://twitter.com/Chad_Napper", "_blank");
     });
-};
 
-for (child of github) {
-    child.addEventListener("click", () => {
+document.getElementById("card-github").addEventListener("click", () => {
         window.open("https://github.com/ChadNaps", "_blank");
     });
-};
 
-for (child of linkedin) {
-    child.addEventListener("click", () => {
+document.getElementById("card-linkedin").addEventListener("click", () => {
         window.open("https://www.linkedin.com/in/chad-napper-66114812a/", "_blank");
     });
-};
